4988ad05ec8cd0995d3d1d99bd6dbfd6cc5aea45
Vitalii Vereta <vitalii.vereta@volvocars.com>
2021-04-04 18:11:35 +0300

    Add tcp_client example

---
+++ libs/connect/connect.c
@@ -0,0 +1,76 @@
+#include <string.h>
+
+#include "connect.h"
+
+#include "esp_wifi.h"
+#include "esp_log.h"
+#include "tcpip_adapter.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+
+#define GOT_IPV4_BIT BIT(0)
+#define CONNECTED_BITS (GOT_IPV4_BIT)
+
+static EventGroupHandle_t s_connect_event_group;
+static ip4_addr_t s_ip_addr;
+static char s_connection_name[32] = CONFIG_EXAMPLE_WIFI_SSID;
+static char s_connection_passwd[32] = CONFIG_EXAMPLE_WIFI_PASSWORD;
+
+static const char *TAG = "lib_connect";
+
+static void on_got_ip(void *arg, esp_event_base_t event_base,
+                      int32_t event_id, void *event_data)
+{
+    ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+    memcpy(&s_ip_addr, &event->ip_info.ip, sizeof(s_ip_addr));
+    xEventGroupSetBits(s_connect_event_group, GOT_IPV4_BIT);
+}
+
+static void on_wifi_disconnect(void *arg, esp_event_base_t event_base,
+                               int32_t event_id, void *event_data)
+{
+    system_event_sta_disconnected_t *event = (system_event_sta_disconnected_t *)event_data;
+
+    ESP_LOGI(TAG, "Wi-Fi disconnected, trying to reconnect...");
+    if (event->reason == WIFI_REASON_BASIC_RATE_NOT_SUPPORT) {
+        /*Switch to 802.11 bgn mode */
+        esp_wifi_set_protocol(ESP_IF_WIFI_STA, WIFI_PROTOCOL_11B | WIFI_PROTOCOL_11G | WIFI_PROTOCOL_11N);
+    }
+    ESP_ERROR_CHECK(esp_wifi_connect());
+}
+
+static void start(void)
+{
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &on_wifi_disconnect, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &on_got_ip, NULL));
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+    wifi_config_t wifi_config = { 0 };
+
+    strncpy((char *)&wifi_config.sta.ssid, s_connection_name, 32);
+    strncpy((char *)&wifi_config.sta.password, s_connection_passwd, 32);
+
+    ESP_LOGI(TAG, "Connecting to %s...", wifi_config.sta.ssid);
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+    ESP_ERROR_CHECK(esp_wifi_connect());
+}
+
+esp_err_t example_connect(void)
+{
+    if (s_connect_event_group != NULL) {
+        return ESP_ERR_INVALID_STATE;
+    }
+
+    s_connect_event_group = xEventGroupCreate();
+    start();
+    xEventGroupWaitBits(s_connect_event_group, CONNECTED_BITS, true, true, portMAX_DELAY);
+    ESP_LOGI(TAG, "Connected to %s", s_connection_name);
+    ESP_LOGI(TAG, "IPv4 address: " IPSTR, IP2STR(&s_ip_addr));
+
+    return ESP_OK;
+}
---
+++ libs/connect/connect.h
@@ -0,0 +1,18 @@
+#include "esp_err.h"
+
+/**
+ * @brief Configure Wi-Fi or Ethernet, connect, wait for IP
+ *
+ * This all-in-one helper function is used in protocols examples to
+ * reduce the amount of boilerplate in the example.
+ *
+ * It is not intended to be used in real world applications.
+ * See examples under examples/wifi/getting_started/ and examples/ethernet/
+ * for more complete Wi-Fi or Ethernet initialization code.
+ *
+ * Read "Establishing Wi-Fi or Ethernet Connection" section in
+ * examples/protocols/README.md for more information about this function.
+ *
+ * @return ESP_OK on successful connection
+ */
+esp_err_t example_connect(void);
